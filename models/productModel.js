79eb0b8ef091f29998b1d96b7d97ba2246b6dcc1
Okkar25 <okkaaung2508@gmail.com>
2025-06-13 03:54:02 -0400

    create products write to file

--- models/productModel.js
+++ models/productModel.js
@@ -1,4 +1,11 @@
 let products = require("../data/products.json");
+const { v4: uuidv4 } = require("uuid");
+const { writeDataToFile } = require("../utils");
+
+// console.log(__dirname);
+// console.log(__dirname.split("\\").slice(0, -1).join("\\"));
+// console.log(__filename);
+const productsFilePath = __dirname.split("\\").slice(0, -1).join("\\") + "\\data\\products.json";
 
 const findAll = () => {
   return new Promise((resolve, reject) => {
@@ -13,7 +20,19 @@ const findById = (id) => {
   });
 };
 
+const create = (product) => {
+  return new Promise((resolve, reject) => {
+    const newProduct = { id: uuidv4(), ...product };
+    products.push(newProduct);
+    writeDataToFile(productsFilePath, products);
+    resolve(newProduct);
+  });
+};
+
+//
+
 module.exports = {
   findAll,
   findById,
+  create,
 };
