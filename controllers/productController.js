291a663fc7fca6386f56b2a1bc1d8bf5a0f46054
Okkar25 <okkaaung2508@gmail.com>
2025-06-13 02:15:53 -0400

    get all products

---
+++ controllers/productController.js
@@ -0,0 +1,19 @@
+const Product = require("../models/productModel");
+
+// @desc    Gets All Products
+// @route   GET /api/products
+
+const getProducts = async (req, res) => {
+  try {
+    const products = await Product.findAll();
+
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify(products));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+module.exports = {
+  getProducts,
+};
---
+++ server.js
@@ -0,0 +1,20 @@
+const http = require("http");
+const { getProducts } = require("./controllers/productController");
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/api/products" && req.method === "GET") {
+    getProducts(req, res);
+  } else {
+    res.writeHead(404, { "Content-Type": "application/json" });
+    res.end(
+      JSON.stringify({
+        message: "Route Not Found: Please use the api/products endpoint",
+      })
+    );
+  }
+});
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
+
